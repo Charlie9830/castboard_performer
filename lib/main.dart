307906b95aa9d200f07e853ab206bf33984e5389
Charlie9830 <charliewjhall@gmail.com>
2021-05-08 14:02:19 +0900

    Custom fonts complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:castboard_core/font-loading/FontLoadCandidate.dart';
+import 'package:castboard_core/font-loading/FontLoading.dart';
+import 'package:castboard_core/font-loading/FontLoadingResult.dart';
 import 'package:castboard_core/models/ActorModel.dart';
+import 'package:castboard_core/models/FontModel.dart';
 import 'package:castboard_core/models/PresetModel.dart';
 import 'package:castboard_core/models/TrackModel.dart';
 import 'package:castboard_core/models/SlideModel.dart';
@@ -32,6 +36,7 @@ class _AppRootState extends State<AppRoot> {
   Map<String, TrackModel> _tracks;
   Map<String, PresetModel> _presets;
   Map<String, SlideModel> _slides;
+  List<FontModel> _unloadedFonts = const <FontModel>[];
   SlideCycler _cycler;
   PresetModel _currentPreset;
   String _currentSlideId = '';
@@ -60,7 +65,6 @@ class _AppRootState extends State<AppRoot> {
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.grey,
-        accentColor: Colors.amber,
       ),
       initialRoute: RouteNames.loadingSplash,
       routes: {
@@ -99,7 +103,6 @@ class _AppRootState extends State<AppRoot> {
   void _handleShowFileReceived() async {
     final data = await Storage.instance.readFromPlayerStorage();
 
-    print('Loading');
     _loadShow(data);
   }
 
@@ -120,7 +123,7 @@ class _AppRootState extends State<AppRoot> {
     }
   }
 
-  void _loadShow(ImportedShowData data) {
+  void _loadShow(ImportedShowData data) async {
     if (_cycler != null) {
       _cycler.dispose();
     }
@@ -133,12 +136,18 @@ class _AppRootState extends State<AppRoot> {
     final currentPreset =
         data.presets.isNotEmpty ? data.presets.values.first : _currentPreset;
 
+    print(data.manifest.requiredFonts);
+    final unloadedFontIds = await _loadCustomFonts(data.manifest.requiredFonts);
+    final fontsLookup = Map<String, FontModel>.fromEntries(
+        data.manifest.requiredFonts.map((font) => MapEntry(font.uid, font)));
+
     setState(() {
       _actors = data.actors;
       _tracks = data.tracks;
       _presets = data.presets;
       _slides = data.slides;
       _currentSlideId = initialSlide?.uid ?? _currentSlideId;
+      _unloadedFonts = unloadedFontIds.map((id) => fontsLookup[id]).toList();
       _cycler = SlideCycler(
           slides: sortedSlides,
           initialSlide: initialSlide,
@@ -148,6 +157,60 @@ class _AppRootState extends State<AppRoot> {
     });
 
     navigatorKey.currentState?.pushNamed(RouteNames.player);
+  }
+
+  ///
+  /// Pulls Custom fonts from storage and loads into the Engine.
+  /// Returns a Set of ids representing any Fonts that could not be loaded.
+  /// This could be because the Engine rejected them, files were missing, FontModel.ref was invalid or bad.
+  Future<Set<String>> _loadCustomFonts(List<FontModel> requiredFonts) async {
+    if (requiredFonts == null || requiredFonts.isEmpty) {
+      return <String>{};
+    }
+
+    final List<FontModel> goodFonts = [];
+    final List<FontModel> missingFonts = [];
+    final existenceRequests = requiredFonts.map((font) =>
+        _fontExistenceDelegate(font).then(
+            (exists) => exists ? goodFonts.add(font) : missingFonts.add(font)));
+
+    await Future.wait(existenceRequests);
+
+    final List<FontLoadCandidate> candidates = [];
+    final dataLoadRequests = goodFonts.map((font) => Storage.instance
+        .getFontFile(font.ref)
+        .readAsBytes()
+        .then((data) => candidates
+            .add(FontLoadCandidate(font.uid, font.familyName, data))));
+
+    await Future.wait(dataLoadRequests);
+
+    final loadingResults = await FontLoading.loadFonts(candidates);
+    print('Good Fonts ${goodFonts.length}');
+    print('Missing Fonts ${missingFonts.length}');
+    print('Unloaded Fonts ${loadingResults.length}');
+
+
+    return [
+      ...loadingResults
+          .where((result) => result.loadResult.success == false)
+          .map((result) => result.uid),
+      ...missingFonts.map((font) => font.uid)
+    ].toSet();
+  }
+
+  ///
+  /// Delegate for checking if a Font File exists. Will return false even if the file object itself is null, thus protecting
+  /// any .then() calls from a null exception.
+  ///
+  Future<bool> _fontExistenceDelegate(FontModel font) async {
+    final file = Storage.instance.getFontFile(font.ref);
+
+    if (file == null) {
+      return false;
+    } else {
+      return file.exists();
+    }
   }
 
   void _handleSlideCycle(String slideId, bool playing) {
