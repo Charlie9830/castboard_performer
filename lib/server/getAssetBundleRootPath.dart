74310a451aa88ac945d0f396f3bfcbcf396b0496
Charlie9830 <charliewjhall@gmail.com>
2021-07-27 22:26:55 +1000

    Temporary workaround for kDebugMode bug

--- lib/server/getAssetBundleRootPath.dart
+++ lib/server/getAssetBundleRootPath.dart
@@ -4,6 +4,12 @@ import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as p;
 
 String getAssetBundleRootPath() {
+  // TODO: Temporary override of kDebugMode behaviour until bug is fixed.
+  if (Platform.isLinux) {
+    // Flutter-Pi Layout
+    return p.join(p.current, 'assets');
+  }
+
   if (kDebugMode) {
     return _getDebugAssetBundleRootPath();
   }
@@ -12,10 +18,11 @@ String getAssetBundleRootPath() {
     return p.join(p.current, 'data', 'flutter_assets', 'assets');
   } 
 
-  if (Platform.isLinux) {
-    // Flutter-Pi Layout
-    return p.join(p.current, 'assets');
-  }
+  // TODO: Re-enable when kDebugMode bug is fixed.
+  // if (Platform.isLinux) {
+  //   // Flutter-Pi Layout
+  //   return p.join(p.current, 'assets');
+  // }
 
   else {
     throw "Platform not currently supported by getAssetBundleRootPath(). Add conditional handling for this platform";
