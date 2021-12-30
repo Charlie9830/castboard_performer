6ca8ea9db251e574666a898fd15668ad20ac44a4
Charlie Hall <charliewjhall@gmail.com>
2021-12-30 21:10:34 +1100

    Updated Application conf path

--- lib/system_controller/platform_implementations/rpi_linux/getAppConfigFIle.dart
+++ lib/system_controller/platform_implementations/rpi_linux/getAppConfigFIle.dart
@@ -1,10 +1,8 @@
 import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'dart:io';
 
 Future<File> getAppConfigFile() async {
-  final supportDir = await pathProvider.getApplicationDocumentsDirectory();
-  final appConfigFile = File(p.join(supportDir.path, 'castboard.conf'));
+  final appConfigFile = File(p.join('etc', 'castboard', 'castboard.conf'));
 
   return appConfigFile;
 }
