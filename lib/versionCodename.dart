0867fdc165b36d048f8bb7a368b20fa657fffeaf
Charlie Hall <charliewjhall@gmail.com>
2022-01-06 10:46:04 +1100

    versionCodename test build

--- lib/versionCodename.dart
+++ lib/versionCodename.dart
@@ -1,3 +1,3 @@
 // BE CAREFUL: The kVersionCodename property is referenced by the python build
 // scripts to build the Player Update artifacts.
-const String kVersionCodename = "Gandalf";
+const String kVersionCodename = "Legolas";
